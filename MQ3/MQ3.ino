584a2a15f48a49e9f3bb9d343451284bf008f5ea
nt63859 <80894233+nt63859@users.noreply.github.com>
2021-03-18 21:59:32 +0800

    Update MQ3.ino

--- MQ3/MQ3.ino
+++ MQ3/MQ3.ino
@@ -1,17 +1,19 @@
 
-int sensorPin = A0;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
-
 void setup() {
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
+ // pinMode(A0, INPUT);
+ 
+  int  adcvalue ;
+  adcvalue =analogRead(A0);
+  analogSetPinAttenuation(A0,ADC_11db);
+  adcAttachPin(A0);
+  adcStart (A0);
+  
 }
 
 void loop() {
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-    Serial.println(sensorValue);
-  
+  int alco;
+  alco = analogRead(A0);
+  Serial.println(alco);
+  delay (100) ;
 }
